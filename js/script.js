3b8a95b42f8d1995f6e53e081b547dc2cd7643b5
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-13 01:17:58 +0100

    completato-3 , aggiungo animation fastidiosa

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
-// Milestone 1 
-// stampo i numeri da uno a cento con eccezioni per i multipli di 3 e di 5 
+
+
+
+
+const container=document.querySelector(".container");
 
 for(let i=1;i<=100;i++){
 
     // 1 -controllo che sia multiplo di 3 e di 5 
     if(i % 3==0 && i % 5==0){
         console.log("FizzBuzz");
+
+        // creo un div con la classe box e  buzzfizz e ci inserisco il numero
+        container.innerHTML+=`<div class="box buzzfizz">${i}`;
     }
     
     //2 - se è multiplo di 3 stampo Fizz
     else if(i%3==0){
         console.log("Fizz");
+
+        // creo un div con la classe box e  fizz e ci inserisco il numero
+        container.innerHTML+=`<div class="box fizz">${i}`;
+       
     }
     //3 - se è multiplo di 5 stampo Buzz
     else if(i%5==0){
         console.log("Buzz");
+        
+        // creo un div con la classe box e  buzz e ci inserisco il numero
+        container.innerHTML+=`<div class="box buzz">${i}`;
     }
     // altrimenti stampo il numero 
     else{
-    console.log(`N == ${i}`);
+        console.log(`${i}`);
+
+        //div con classe di default
+        container.innerHTML+=`<div class="box">${i}`;
     }
 }
+
+
+
+
+
